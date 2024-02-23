15e63403565b5f8cd292148ed9ac1b4a35a8d0a9
Akshay S <62248044+blitzster@users.noreply.github.com>
2024-02-23 15:33:08 +0000

    datatypes

---
+++ 01Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  //treats all JS code as newer version
+
+
+let name = "Akshay"
+let age = 24
+let isLoggedIn = true
+let state;
+
+
+
+// number -> 2 to power 52
+// bigInt
+// string -> ""
+// boolean -> true/false
+// null -> standalone value // null is an object
+// undefined -> let state
+// symbol -> unique
+
+// object 
+
+console.log(typeof undefined)
+console.log(typeof null)
