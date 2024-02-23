4e5143d12e5b3a49c8d4c9ba936f9174163af659
Akshay S <62248044+blitzster@users.noreply.github.com>
2024-02-23 15:47:11 +0000

    datatype conversion

---
+++ 01Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "69"
+
+//console.log(typeof score)
+
+let value = Number(score)
+//console.log(typeof value);
+//console.log(value);
+
+//if we try to convert "69abc" then it wont work obviously cause it is a string and it will print NaN
+
+//if we try to convert null then the output will be 0
+
+// if it is undefined then the output will be NaN
+
+// if it is boolean values then the output will be 1 for true and 0 if the value is false
+
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Akshay" => true
