7201f8365210daf88274d5fad7bebf3d7d1cdb52
Akshay S <62248044+blitzster@users.noreply.github.com>
2024-02-24 14:20:55 +0000

    updated

--- 01Basics/datatypes-summary.js
+++ 01Basics/datatypes-summary.js
@@ -30,3 +30,35 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction) // this will return function where it is the type of object function
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack(Primitive) and heap(non-primitive)
+
+// Stack
+//In stack it does not change the values cause if only modifes the copy of the variable and the original variable will still be in the stack
+//so when we call the variable the original variable from the stack will be printed which will be different to the other variable
+let myYoutubeName = "Blitzster"
+
+let anotherName = myYoutubeName
+anotherName = "Akshay"
+
+console.log(myYoutubeName)
+console.log(anotherName);
+
+
+//HEAP
+//When we change the data in heap all the elements will be affected as they are referenced to the same heap memory from stack
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "akshay@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
